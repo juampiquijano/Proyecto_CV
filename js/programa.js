d31caaf576525b4b95f7b662d912b5e4e3797c71
Juan Pablo Quijano <juanpquijano@outlook.com>
2023-01-12 20:45:40 -0300

    Modificacion del excel y del programa js

--- js/programa.js
+++ js/programa.js
@@ -12,15 +12,19 @@ $.ajax({
 
 let url2 = "info.txt";
 let url3 = "C:\Users\Juan Pablo Quijano\OneDrive - fi.uba.ar\Curso Front End -Ticmas Academy\Proyecto CV\js\info.txt"
+let url4 = "https://raw.githubusercontent.com/juampiquijano/Proyecto_CV/master/js/info.txt"
+
 /**
 fetch (url2)
 .then(texto => texto.text())
 .then(texto2 => console.log(texto2));
  */
 
-fetch(url3)
-  .then(res => res.text())
-  .then(content => {
+fetch(url4)
+  .then(response => response.text())
+  .then(content => document.getElementById("archivo").innerHTML = content)
+/**
+  {
     let lines = content.split(/\n/);
     lines.forEach(line => console.log(line));
-  });
+  }); */
