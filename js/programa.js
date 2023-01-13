f3458670b7bdb305f7773679091cdd79fac402f1
Juan Pablo Quijano <juanpquijano@outlook.com>
2023-01-13 05:10:27 -0300

    JS: Se agregó una función para crear tarjetas autocompletadas y una función para autocompletar la información personal. Excel: Se completó con los datos personales y de experiencia para poder extraer la información en formato JSON con un script propio de excel. HTML: Se quitaron las tarjetas para que se completen directamente con el código cuando se abre la página.

--- js/programa.js
+++ js/programa.js
@@ -1,30 +1,43 @@
-const url1 = 'https://randomuser.me/api/'
+const url = "https://raw.githubusercontent.com/juampiquijano/Proyecto_CV/master/js/info.txt"
 
-/*
-$.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data) {
-      console.log(data);
-    }
-  });
-*/
-
-let url2 = "info.txt";
-let url3 = "C:\Users\Juan Pablo Quijano\OneDrive - fi.uba.ar\Curso Front End -Ticmas Academy\Proyecto CV\js\info.txt"
-let url4 = "https://raw.githubusercontent.com/juampiquijano/Proyecto_CV/master/js/info.txt"
-
-/**
-fetch (url2)
-.then(texto => texto.text())
-.then(texto2 => console.log(texto2));
- */
-
-fetch(url4)
+/* Pido la información al servidor */
+fetch(url)
   .then(response => response.text())
-  .then(content => document.getElementById("archivo").innerHTML = content)
-/**
-  {
-    let lines = content.split(/\n/);
-    lines.forEach(line => console.log(line));
-  }); */
+  .then(info => {
+    info = info.split("\n")
+    let info_personal = JSON.parse(info[0])[0]
+    let info_exp = JSON.parse(info[1])
+    carga_info(info_personal);
+    for (var i in info_exp) {
+      console.log(info_exp[i])
+      crear_tarjeta(info_exp[i]);
+    }
+  })
+
+/*Reemplaza la indicación genérica de información personal por los valores reales*/
+function carga_info(obj) {
+  for (var key in obj) {
+    try {
+      document.getElementById(key).innerHTML = obj[key];
+    } catch (error) {
+      console.log("Error con la clave '" + key + "': No existe el id '" + key +"'. " + error.message);
+    }
+  }
+}
+
+/* Crea una tarjeta genérica y la completa con los datos reales */
+function crear_tarjeta(obj) {
+  let tarjeta
+  tarjeta =  `<div class="col-md-6">
+                <div class="card">
+                    <h4 class="card-header titulo">`+ obj.Titulo + 
+                    `<small class="text-muted"> - `+ obj.Lugar +`</small></h4>
+                    <div class="card-body">
+                        <p class="card-text periodo">`+ obj.Periodo +`</p>
+                        <p class="card-text descripcion">`+ obj.Descripcion +`</p>
+                    </div>
+                </div>
+              </div>`
+    let seccion = document.getElementById("s_"+ obj.Tipo);
+    seccion.insertAdjacentHTML("beforeend", tarjeta)
+}
